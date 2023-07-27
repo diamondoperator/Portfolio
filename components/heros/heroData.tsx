742b5ae218292f45a81252447d84d4e9266d3ad6
Bryant Davis <hackerman20xx@gmail.com>
2023-07-27 17:18:44 -0400

    Update heroData.tsx
    edited social links

--- components/heros/heroData.tsx
+++ components/heros/heroData.tsx
@@ -4,7 +4,7 @@ import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 export const heroData = [
   {
     label: "github",
-    href: "https://github.com/itsjagdeep",
+    href: "https://github.com/diamondoperator/", //"https://github.com/itsjagdeep",
     style:
       "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300",
     icon: <FaGithub />,
@@ -16,7 +16,7 @@ export const heroData = [
       "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-600",
     icon: <FaLinkedinIn />,
   },
-  {
+ /* {
     label: "twitter",
     href: "https://twitter.com/i/flow/login?redirect_after_login=%2F%3Flang%3Den",
     style:
@@ -30,5 +30,5 @@ export const heroData = [
     style:
       "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-pink-500",
     icon: <BsFillPersonLinesFill />,
-  },
+  },*/
 ] as const;
