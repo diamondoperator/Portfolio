43b1176bd07ae1ead3b842aeed0d0579f01482d8
Bryant Davis <hackerman20xx@gmail.com>
2023-08-01 16:36:56 -0400

    change url link

--- components/contacts/ContactImage.tsx
+++ components/contacts/ContactImage.tsx
@@ -10,7 +10,7 @@ export default function ContactImage(): JSX.Element {
         className="rounded-xl md:flex hidden"
         height={1080}
         placeholder="blur"
-        src="https://source.unsplash.com/random/?vintagecomputer"
+        src="https://source.unsplash.com/random/?computer"
         width={1920}
       />
     </div>
