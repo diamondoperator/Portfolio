189fa15a391470ed0e33528eb34fe2fc548326cc
Bryant Davis <hackerman20xx@gmail.com>
2023-08-03 16:00:57 -0400

    Update heroData.tsx
    change background to red

--- components/heros/heroData.tsx
+++ components/heros/heroData.tsx
@@ -10,10 +10,9 @@ export const heroData = [
     icon: <FaGithub />,
   },
   {
-    label: "linkedin",
-    href: "https://www.linkedin.com/in/bryant-davis-a3028726", //"https://www.linkedin.com/in/thisisjagdeep/",
-    style:
-      "bg-white-500 rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-400",
-    icon: <FaLinkedinIn />,
-  },
+  label: "linkedin",
+  href: "https://www.linkedin.com/in/bryant-davis-a3028726", //"https://www.linkedin.com/in/thisisjagdeep/",
+  style: "bg-red-500 rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-400",
+  icon: <FaLinkedinIn />,
+  }
 ] as const;
