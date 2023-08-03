28a547bb7371e27286d19c3d7b9de838d1f15004
Bryant Davis <hackerman20xx@gmail.com>
2023-08-03 15:59:13 -0400

    Update heroData.tsx
    add white background

--- components/heros/heroData.tsx
+++ components/heros/heroData.tsx
@@ -6,14 +6,14 @@ export const heroData = [
     label: "github",
     href: "https://github.com/diamondoperator/", //"https://github.com/itsjagdeep",
     style:
-      "rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300",
+      "bg-white-500 rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300",
     icon: <FaGithub />,
   },
   {
     label: "linkedin",
     href: "https://www.linkedin.com/in/bryant-davis-a3028726", //"https://www.linkedin.com/in/thisisjagdeep/",
     style:
-      "rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-400",
+      "bg-white-500 rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-400",
     icon: <FaLinkedinIn />,
   },
 ] as const;
