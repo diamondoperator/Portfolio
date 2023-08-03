6301c96cdd3da9ca4728ce9173b1c25954c4f790
Bryant Davis <hackerman20xx@gmail.com>
2023-08-03 15:36:01 -0400

    Update skillsData.tsx
    add skills

--- components/skill/skillsData.tsx
+++ components/skill/skillsData.tsx
@@ -47,6 +47,24 @@ export const skillsData = [
     icon: <SiGit />,
     h3: "Git",
   },
+  {
+    href: "https://github.com/milliorn",
+    style: "m-auto p-1 sm:text-5xl text-red-500",
+    icon: <SiGit />,
+    h3: "Physical Computing",
+  },
+  {
+    href: "https://github.com/milliorn",
+    style: "m-auto p-1 sm:text-5xl text-red-500",
+    icon: <SiGit />,
+    h3: "Creative Coding",
+  },
+  {
+    href: "https://github.com/milliorn",
+    style: "m-auto p-1 sm:text-5xl text-red-500",
+    icon: <SiGit />,
+    h3: "New Media",
+  },
   /*{
     href: "https://github.com/milliorn?tab=repositories&q=react&type=public&language=javascript&sort=",
     style: "m-auto p-1 text-blue-500 sm:text-5xl",
