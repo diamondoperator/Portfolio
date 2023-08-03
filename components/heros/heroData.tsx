fb9f56e63c658f1144f1fe44e0a6e039290764e2
Bryant Davis <hackerman20xx@gmail.com>
2023-08-03 16:18:48 -0400

    Update heroData.tsx
    change icon background

--- components/heros/heroData.tsx
+++ components/heros/heroData.tsx
@@ -12,7 +12,7 @@ export const heroData = [
   {
   label: "linkedin",
   href: "https://www.linkedin.com/in/bryant-davis-a3028726", //"https://www.linkedin.com/in/thisisjagdeep/",
-  style: "bg-red-500 rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-400",
+  style: "bg-white rounded-full shadow-lg shadow-white-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-400",
   icon: <FaLinkedinIn />,
   }
 ] as const;
