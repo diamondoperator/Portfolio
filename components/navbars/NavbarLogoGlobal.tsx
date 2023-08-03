28719e550fd9300c32d08f5075de92663986491b
Bryant Davis <hackerman20xx@gmail.com>
2023-08-03 15:42:25 -0400

    Update NavbarLogoGlobal.tsx
    change header logo to text

--- components/navbars/NavbarLogoGlobal.tsx
+++ components/navbars/NavbarLogoGlobal.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import NavLogo from "../../public/assets/navbarLogo.png";
 
-// global navbar logo
+// global navbar logo --original logo <Image src={NavLogo} alt="/" className="cursor-pointer w-20" />
 export default function NavbarLogoGlobal(): JSX.Element {
   return (
     <Link href="/">
-      <Image src={NavLogo} alt="/" className="cursor-pointer w-20" />
+      <h1>Bryant Davis</h1>
     </Link>
   );
 }
